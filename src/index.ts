14b1e3fee2e4fbe919362d15b57dbcd332f28a33
minoru-github <kurami0621@yahoo.co.jp>
2022-06-22 22:08:08 +0900

    style: キャメルケースに統一

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import { LoaderUtils } from 'three';
 import { PCDLoader } from "three/examples/jsm/loaders/PCDLoader"
 console.log("Hello World!");
 
-const input_files = document.getElementById("input_files") as HTMLElement;
+const inputFiles = document.getElementById("input_files") as HTMLElement;
 const text = document.getElementById("text") as any;
 
-input_files.addEventListener("change", function (event: any) {
+inputFiles.addEventListener("change", function (event: any) {
     // https://github.com/fastlabel/AutomanTools/blob/bf1fe121298a88443afdb64fc5d3527553dc8da0/src/web-app/repositories/project-web-repository.ts#L24
     
     var files = event.target.files as FileList;
     const file = files[0];
     const promise = createDataURL(file);
     promise.then((path) => {
-        load_pcd(path);
+        loadPcd(path);
     });
 
     });
@@ -60,22 +60,22 @@ function extractData(pcdFile: string) {
     // TODO:DATAの検索を正規表現に変える
     const headerOfData = "DATA ascii\n";
     const beginOfData = pcdFile.indexOf(headerOfData) + headerOfData.length;
-    const data_vec = pcdFile.slice(beginOfData).split("\n");
+    const dataVec = pcdFile.slice(beginOfData).split("\n");
     
-    const xyz_vec = new Array<XYZ>;
+    const xyzVec = new Array<XYZ>;
     for (var cnt = 0; cnt < points; cnt++){
-        const data = data_vec[cnt].split(" ");
+        const data = dataVec[cnt].split(" ");
         const xyz: XYZ = { x: parseFloat(data[0]), y: parseFloat(data[1]), z: parseFloat(data[2]) };
-        xyz_vec.push(xyz);
+        xyzVec.push(xyz);
     }
 
-    function debug(xyz_vec:Array<XYZ>) {
+    function debug(xyzVec:Array<XYZ>) {
         for (var cnt = 0; cnt < 10; cnt++) {
-            console.log("x:%f, y:%f, z:%f", xyz_vec[cnt].x, xyz_vec[cnt].y, xyz_vec[cnt].z);
+            console.log("x:%f, y:%f, z:%f", xyzVec[cnt].x, xyzVec[cnt].y, xyzVec[cnt].z);
         }
     }
 
-    return xyz_vec;
+    return xyzVec;
 }
 
 window.addEventListener('DOMContentLoaded', init);
@@ -85,13 +85,13 @@ const height = 540;
 let scene:THREE.Scene;
 function init() {
     // レンダラーを作成
-    const renderer = create_renderer();
+    const renderer = createRenderer();
 
     // シーンを作成
     scene = new THREE.Scene();
 
     // カメラを作成
-    const camera = create_camera();
+    const camera = createCamera();
 
     // グリッド追加
     const gridHelper = new THREE.GridHelper(200, 40);
@@ -102,13 +102,13 @@ function init() {
     scene.add(axesHelper);
 
     // 箱を作成
-    //const box = create_box();
+    //const box = createBox();
 
     // pcd
     //load_pcd();
 
     // 平行光源
-    create_light();
+    createLight();
 
     // 初回実行
     tick();
@@ -117,14 +117,14 @@ function init() {
         requestAnimationFrame(tick);
 
         // 箱を回転させる
-        //rotate_box(box);
+        //rotateBox(box);
 
         // レンダリング
         renderer.render(scene, camera);
     }
 }
 
-function create_renderer() {
+function createRenderer() {
     const renderer = new THREE.WebGLRenderer({
         canvas: document.querySelector('#myCanvas') as HTMLCanvasElement
     });
@@ -133,14 +133,14 @@ function create_renderer() {
     return renderer;
 }
 
-function create_camera() {
+function createCamera() {
     const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
     camera.position.set(0, 100, 100);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     return camera;
 }
 
-function create_box() {
+function createBox() {
     const geometry = new THREE.BoxGeometry(10, 10, 10);
     const material = new THREE.MeshStandardMaterial({ color: 0x00FFFF });
     const box = new THREE.Mesh(geometry, material);
@@ -149,12 +149,12 @@ function create_box() {
     return box;
 }
 
-function rotate_box(box: THREE.Mesh) {
+function rotateBox(box: THREE.Mesh) {
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
 }
 
-function create_light() {
+function createLight() {
     const light = new THREE.DirectionalLight(0xFFFFFF);
     light.intensity = 2; // 光の強さを倍に
     light.position.set(1, 1, 1);
@@ -162,7 +162,7 @@ function create_light() {
     return light;
 }
 
-function load_pcd(path:string) {
+function loadPcd(path:string) {
     const loader = new PCDLoader();
     loader.load(
         path,
