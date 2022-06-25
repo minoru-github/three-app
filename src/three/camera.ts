550c8bee468bb1aa4bcbb86cf08f7e8cd4df3608
minoru-github <kurami0621@yahoo.co.jp>
2022-06-25 23:54:47 +0900

    pref: カメラ視点の変更

--- src/three/camera.ts
+++ src/three/camera.ts
@@ -8,16 +8,18 @@ const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 export function createCamera() {
     const perspectiveCamera = () => {
         const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 1, 10000);
-        camera.position.set(10, 20, 10);
+        camera.position.set(10, 20, -10);
         return camera;
     }
     const orthographicCamera = () => {
         const camera = new THREE.OrthographicCamera(-canvas.width / 20, canvas.width / 20, canvas.height / 20, -canvas.height / 20, -1000, 1000);
-        camera.position.set(0, 1, 0);
+        camera.position.set(1, 2, -1);
         return camera;
     }
 
+    //const camera = perspectiveCamera();
     const camera = orthographicCamera();
+
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     return camera;
 }
