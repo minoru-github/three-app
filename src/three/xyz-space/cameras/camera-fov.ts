d69e35124dd72625d4c74e76c52ef76e7d4d2f4c
minoru-github <kurami0621@yahoo.co.jp>
2022-07-11 23:56:34 +0900

    feat: fovをlineで描画

--- src/three/xyz-space/cameras/camera-fov.ts
+++ src/three/xyz-space/cameras/camera-fov.ts
@@ -9,7 +9,7 @@ export function drawCameraFov() {
 
 function addCameraMeshToScene(x: number, y: number, z: number) {
     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF0000});
+    const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
     const box = new THREE.Mesh(geometry, material);
     box.name = "camera";
     box.position.set(x, y, z);
@@ -17,15 +17,38 @@ function addCameraMeshToScene(x: number, y: number, z: number) {
 }
 
 function drawImageArea(x: number, y: number, z: number) {
-    const fovRad = cameraCalib.halfFovHorizontal * (Math.PI / 180);
     const distance = 10;
-    const height = 2 * distance * Math.tan(fovRad);
-    const width = height * cameraCalib.aspect;
-    const geometry = new THREE.PlaneGeometry(width, height);
+    const height = 2 * (cameraCalib.height * distance) / cameraCalib.fy_pix;
+    const width = 2 * (cameraCalib.width * distance) / cameraCalib.fx_pix;
+    console.log("imageArea w:{}, h:{}", width, height);
 
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF00FF , wireframe:true});
-    const box = new THREE.Mesh(geometry, material);
-    box.name = "imageArea";
-    box.position.set(x, y, z + distance);
-    get3dSpaceSceneInstance().add(box);
+    const left = cameraCalib.posX + width / 2;
+    const right = cameraCalib.posX - width / 2;
+    const down = cameraCalib.posY - height / 2;
+    const up = cameraCalib.posY + height / 2;
+    const leftdown = new THREE.Vector3(left, down, z + distance);
+    const leftUp = new THREE.Vector3(left, up, z + distance);
+    const rightdown = new THREE.Vector3(right, down, z + distance);
+    const rightUp = new THREE.Vector3(right, up, z + distance);
+    const cameraPos = new THREE.Vector3(x, y, z);
+
+    const points = [];
+    points.push(leftdown);
+    points.push(leftUp);
+    points.push(rightUp);
+    points.push(rightdown);
+
+    points.push(cameraPos);
+    points.push(leftdown);
+    points.push(cameraPos);
+    points.push(leftUp);
+    points.push(cameraPos);
+    points.push(rightUp);
+    points.push(cameraPos);
+    points.push(rightdown);
+
+    const material = new THREE.LineBasicMaterial({ color: 0xFF00FF });
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    get3dSpaceSceneInstance().add(line);
 }
