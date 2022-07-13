7f08660415c95d96d59c51aeebd6c713080025e9
minoru-github <kurami0621@yahoo.co.jp>
2022-07-13 23:12:46 +0900

    feat: 右画像入力時にキャンバスに追加

--- src/three/rgb-image/rgb-image.ts
+++ src/three/rgb-image/rgb-image.ts
@@ -1,14 +1,20 @@
 import { drawCameraFov } from "../xyz-space/camerasThreeJS/camera-fov";
-import { text } from "../../html/element";
 
 export function drawRgbImages(file: File) {
-    // TODO : 入力順で左右を決定しているのを汎用的な仕組みに変える
+    const result = file.name.match(/left|right/);
+    if (result == null) {
+        return;
+    }
+
+    const leftOrRight = result[0];
+    if (leftOrRight == "left") {
+        drawCameraFov();
+    }
+
     const promise = createDataURL(file);
     promise.then((path: string) => {
-        setImageToCanvas(path);
+        setImageToCanvas(path, leftOrRight);
     })
-
-    drawCameraFov();
 
     function createDataURL(file: File) {
         const promise = new Promise<string>((resolve, reject) => {
@@ -23,15 +29,15 @@ export function drawRgbImages(file: File) {
         return promise;
     }
 
-    function setImageToCanvas(path: string) {
+    function setImageToCanvas(path: string, leftOrRight:string) {
         const image = new Image();
         image.src = path;
         image.onload = function () {
-            const canvas = document.getElementById("leftImage") as HTMLCanvasElement;
+            const imageType = leftOrRight + "Image";
+            const canvas = document.getElementById(imageType) as HTMLCanvasElement;
             let context = canvas.getContext("2d");
             canvas.width = image.width;
             canvas.height = image.height;
-            text.value = "canvas width " + canvas.width + "\n";
             if (context != null) {
                 context.drawImage(image, 0, 0);
             }
